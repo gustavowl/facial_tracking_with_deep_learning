ee1176086a5ca841fd32be893c9172d601d71c63
gustavowl <gustavoalvesbezerra@gmail.com>
2018-06-15 17:28:35 -0300

    Bounding Box and Facial Points in videos

--- video_detect_face.py
+++ video_detect_face.py
@@ -23,27 +23,40 @@ def draw_bounding_boxes(img, boxes, edge_width):
 			(0, 255, 0), edge_width )
 
 def draw_facial_points(img, points, square_size):
-	#TODO: verify if points are out of image range when drawing
-	left = -1 * (square_size // 2)
-	right = square_size + left
-	
-	for i in range(len(points[0])):
-		for j in range(len(points) // 2):
-			x = int(round(points[j][i]))
-			y = int(round(points[j + 5][i]))
+	if len(points > 0):
+		#TODO: verify if points are out of image range when drawing
+		left = -1 * (square_size // 2)
+		right = square_size + left
+		
+		for i in range(len(points[0])):
+			for j in range(len(points) // 2):
+				x = int(round(points[j][i]))
+				y = int(round(points[j + 5][i]))
 
-			cv2.rectangle(img, (x + left, y + left), 
-				(x + right, y + right), (0, 255, 0),
-				thickness = cv2.FILLED)
+				cv2.rectangle(img, (x + left, y + left), 
+					(x + right, y + right), (0, 255, 0),
+					thickness = cv2.FILLED)
 
-if (len(sys.argv) == 3):
-	capture = cv2.VideoCapture(sys.argv[2])
+if (len(sys.argv) == 2):
+	with tf.Graph().as_default():
+		gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
+		sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,
+			log_device_placement=False))
+		with sess.as_default():
+			pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
+
+	capture = cv2.VideoCapture(sys.argv[1])
 
 	while (capture.isOpened()):
 		ret, frame = capture.read()
 
 		if ret == True:
-			cv2.line(frame, (0, 0), (50, 50), (0, 0, 255), 5)
+			boxes, points = detect_face.detect_face(frame, MINSIZE,
+				pnet, rnet, onet, THRESHOLD, FACTOR)
+
+			draw_bounding_boxes(frame, boxes, 3)
+			draw_facial_points(frame, points, 5)
+			
 			cv2.imshow(sys.argv[2], frame)
 
 			if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -59,27 +72,7 @@ if (len(sys.argv) == 3):
 	
 	cv2.destroyAllWindows()
 
-	with tf.Graph().as_default():
-		gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
-		sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,
-			log_device_placement=False))
-		with sess.as_default():
-			pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
-
-	img = cv2.imread(sys.argv[1])
-
-	total_boxes, points = detect_face.detect_face(img, MINSIZE, pnet, rnet, onet,
-		THRESHOLD, FACTOR)
-
-	draw_bounding_boxes(img, total_boxes, 3)
-	draw_facial_points(img, points, 5)
-
-	cv2.imshow('dps', img)
-	print("\n\nPress \'Any Key\' to exit")
-	cv2.waitKey(0)
-
 else:
 	print(BREAK_LINE)
-	print("ERROR! 2 arguments expected:\n" + 
-		"\t1 - Image filepath" +
-		"\t2 - Video filepath")
+	print("ERROR! 1 argument expected:\n" + 
+		"\t1 - Video filepath")
