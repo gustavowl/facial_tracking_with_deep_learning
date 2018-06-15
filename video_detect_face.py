92821b54227868a9e80c1b6577d6b619a800537b
gustavowl <gustavoalvesbezerra@gmail.com>
2018-06-15 14:58:20 -0300

    Implements draw_bounding_boxes and draw_facial_points using opencv

--- video_detect_face.py
+++ video_detect_face.py
@@ -2,7 +2,8 @@ import sys
 import detect_face
 import tensorflow as tf
 from scipy import misc
-from PIL import Image, ImageDraw
+import cv2
+import numpy as np
 
 
 MINSIZE = 10 # minimum size of face
@@ -12,38 +13,52 @@ FACTOR = 0.709 # scale factor
 BREAK_LINE = "\n\n---------------------------------------\n\n"
 
 def draw_bounding_boxes(img, boxes, edge_width):
-	draw = ImageDraw.Draw(img)
-
 	for i in range(len(boxes)):
 		box = []
 		for j in range(4):
 			box.append(int(round(boxes[i][j])))
 		
 		#TODO: verify if edge is out of image range
-		draw.line( [ (box[0], box[1]), (box[2], box[1]),
-			(box[2], box[3]), (box[0], box[3]), (box[0], box[1]) ],
-			fill = (0, 255, 0), width = edge_width  )
-	
-	del draw
-	return img
+		cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]),
+			(0, 255, 0), edge_width )
 
 def draw_facial_points(img, points, square_size):
 	#TODO: verify if points are out of image range when drawing
-	mask_left = -1 * (square_size // 2)
-	mask_right = square_size + mask_left
-	pixels = img.load()
+	left = -1 * (square_size // 2)
+	right = square_size + left
 	
 	for i in range(len(points[0])):
 		for j in range(len(points) // 2):
-			for x in range(mask_left, mask_right):
-				for y in range(mask_left, mask_right):
-					pixels[x + round(float(points[j][i])),
-					y + round(float(points[j + 5][i]))] = (0, 255, 0)
+			x = int(round(points[j][i]))
+			y = int(round(points[j + 5][i]))
+
+			cv2.rectangle(img, (x + left, y + left), 
+				(x + right, y + right), (0, 255, 0),
+				thickness = cv2.FILLED)
+
+if (len(sys.argv) == 3):
+	capture = cv2.VideoCapture(sys.argv[2])
+
+	while (capture.isOpened()):
+		ret, frame = capture.read()
+
+		if ret == True:
+			cv2.line(frame, (0, 0), (50, 50), (0, 0, 255), 5)
+			cv2.imshow(sys.argv[2], frame)
+
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+		
+		else:
+			break
+
+	capture.release()
+
+	print("\n\nPress \'Enter\' to exit")
+	input()
 	
-	return img
+	cv2.destroyAllWindows()
 
-
-if (len(sys.argv) == 2):
 	with tf.Graph().as_default():
 		gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
 		sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,
@@ -51,26 +66,20 @@ if (len(sys.argv) == 2):
 		with sess.as_default():
 			pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
 
-	print(BREAK_LINE)
-
-	filepath = sys.argv[1]
-	img = misc.imread(filepath)
+	img = cv2.imread(sys.argv[1])
 
 	total_boxes, points = detect_face.detect_face(img, MINSIZE, pnet, rnet, onet,
 		THRESHOLD, FACTOR)
 
-	print("\n\n\n\nEITA\n\n\n\n")
-	print(total_boxes)
-	print(len(total_boxes))
-	print("\n\n")
-	print(points)
+	draw_bounding_boxes(img, total_boxes, 3)
+	draw_facial_points(img, points, 5)
 
-	pilimg = Image.open(filepath)
-	pilimg = draw_bounding_boxes(pilimg, total_boxes, 5)
-	pilimg = draw_facial_points(pilimg, points, 5)
-	pilimg.show()
+	cv2.imshow('dps', img)
+	print("\n\nPress \'Any Key\' to exit")
+	cv2.waitKey(0)
 
 else:
 	print(BREAK_LINE)
-	print("ERROR! 1 argument expected:\n" + 
-		"\t1 - Image filepath")
+	print("ERROR! 2 arguments expected:\n" + 
+		"\t1 - Image filepath" +
+		"\t2 - Video filepath")
